52758f7fbfc0d989e17eab52498f92b8fcf06d78
Bella Wang <sunry81914@gmail.com>
2017-06-25 18:30:03 +0800

    init django-food-ig

---
+++ mysite/trips/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from datetime import datetime
+from .models import Post
+
+# Create your views here.
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': str(datetime.now()),
+    })
+
+def home(request):
+	post_list = Post.objects.all()
+	return render(request, 'home.html', { 'post_list': post_list })
+
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'post.html', { 'post': post })
